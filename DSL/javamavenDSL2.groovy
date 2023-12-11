7a30a24d0e2e28c0507e1f26fc56a029673ace88
Adrian Moreno Monterde <amoreno@stemdo.io>
2023-12-11 10:52:06 +0100

    Actualizacion del SCM de los groovys con los valores de mi usuario de github

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/visumania/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('visumania')
+            node / gitConfigEmail('adrian.moreno.mon@gmail.com')
         }
     }
     steps {
